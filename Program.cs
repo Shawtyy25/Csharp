314bde6d23d2abb9ab03c88cf437431e70c31a29
zsabesz <szolnokiabi25@gmail.com>
2024-09-06 09:13:07 +0200

    Argumentumok

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Csharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Az első argumentum: "+ args[0]);
+        Console.Write("CICA");
+        System.Console.WriteLine("Cica");
+    }
+    
+}
